b40173cb275b76fdc94bdaa61627e11c4de31fe1
enochyizhe <enochyizhe@foxmail.com>
2018-03-21 14:58:05 +0800

    第一次问候

--- basic/controllers/SiteController.php
+++ basic/controllers/SiteController.php
@@ -129,6 +129,6 @@ class SiteController extends Controller
 
     public function actionSay($message = 'Hello')
     {
-        return $this->render('say',['message => $message']);
+        return $this->render('say',['message' => $message]);
     }
 }
